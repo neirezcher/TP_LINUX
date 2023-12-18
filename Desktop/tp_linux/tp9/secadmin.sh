51ad1e564cbf2bc1010949562dbee5b59f316f86
neirezcher <100490338+neirezcher@users.noreply.github.com>
2023-12-18 16:10:50 +0100

    Update secadmin.sh

--- Desktop/tp_linux/tp9/secadmin.sh
+++ Desktop/tp_linux/tp9/secadmin.sh
@@ -45,9 +45,80 @@ case $choice in
 
 	sudo tcpdump -i $interface
 	;;
-5)	
+5)	# Vérifier les droits d'administration
+	if [[ $EUID -ne 0 ]]; then
+    		echo "Ce script doit être exécuté en tant qu'administrateur (root)." 
+    	exit 1
+	fi
+
+	# Afficher le menu
+	echo "=== Gestion du Firewall (iptables) ==="
+	echo "51. Autoriser le trafic sur un port"
+	echo "52. Bloquer le trafic sur un port"
+	echo "53. Autoriser toutes les connexions sortantes"
+	echo "54. Bloquer toutes les connexions sortantes"
+	echo "55. Afficher les règles iptables actuelles"
+	echo "56. Quitter"
+
+	# Lire le choix de l'utilisateur
+	read -p "Entrez le numéro de l'option souhaitée : " choice
+
+	# Utiliser la structure conditionnelle case pour traiter le choix de l'utilisateur
+	case $choice in
+    		1)
+       		 	# Autoriser le trafic sur un port
+        		read -p "Entrez le numéro du port à autoriser : " port
+        		iptables -A INPUT -p tcp --dport $port -j ACCEPT
+        		echo "Le trafic sur le port $port est autorisé."
+        		;;
+    		2)
+        		# Bloquer le trafic sur un port
+        		read -p "Entrez le numéro du port à bloquer : " port
+        		iptables -A INPUT -p tcp --dport $port -j DROP
+        		echo "Le trafic sur le port $port est bloqué."
+        		;;
+    		3)
+        		# Autoriser toutes les connexions sortantes
+        		iptables -P OUTPUT ACCEPT
+        		echo "Toutes les connexions sortantes sont autorisées."
+        		;;
+    		4)
+        		# Bloquer toutes les connexions sortantes
+        		iptables -P OUTPUT DROP
+        		echo "Toutes les connexions sortantes sont bloquées."
+        		;;
+    		5)
+        		# Afficher les règles iptables actuelles
+        		echo "Règles iptables actuelles :"
+        		iptables -L
+        		;;
+    		6)
+        		exit 0
+        		;;
+    		*)
+        		# Gérer les choix non valides
+        		echo "Option non valide. Veuillez choisir un numéro entre 1 et 6."
+        		;;
+	esac
+
+	# Enregistrement des règles iptables
+	service iptables save
+	echo "Les règles iptables ont été enregistrées."
+
+	# Redémarrer le service iptables
+	service iptables restart
+	echo "Le service iptables a été redémarré."
 	;;
-6)	
+6)	# Utilisation de la commande find pour rechercher les fichiers avec le bit SUID
+	suid_files=$(find / -type f -perm /4000 2>/dev/null)
+
+	# Vérifier si des fichiers SUID ont été trouvés
+	if [ -n "$suid_files" ]; then
+    		echo -e "\nFichiers avec le bit SUID trouvés :"
+    		echo "$suid_files"
+	else
+   		echo "Aucun fichier avec le bit SUID trouvé."
+	fi
 	;;
 7)	
 	;;
